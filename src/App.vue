02f63314a8955133d988b334d26ca30157a58236
kadyrov23 <zloyrudik@mail.ru>
2023-05-15 21:46:36 +0500

    header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+</script>
 
 <template>
-  <div class="wrapper">
-    <div class="header">
-      <el-link type="primary" @click="$router.push({ name: 'List' })"
-        >Главная</el-link
-      >
-      <!-- <el-link type="success" @click="$router.push({ name: 'Product' })"
-        >Товар</el-link
-      > -->
-    </div>
-    <div class="app__main">
-      <router-view></router-view>
-    </div>
+  <Header />
+  <div class="app__main">
+    <router-view></router-view>
   </div>
 </template>
 
 <style scoped lang="scss">
-.header {
-  background-color: #d9ecff;
-  position: fixed;
-  top: 0;
-  z-index: 100;
-  width: 100%;
-  padding: 16px;
-  border-bottom: 2px solid #409eff;
-}
-
 .app__main {
   margin-top: 60px;
-}
-.el-link {
-  font-size: 20px;
-  margin-right: 20px;
 }
 </style>

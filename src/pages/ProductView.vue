7db0c04550e4e4ec79f647a2e6c5c1e7abbb52e9
kadyrov23 <zloyrudik@mail.ru>
2023-05-19 23:33:25 +0500

    request on detail page

--- src/pages/ProductView.vue
+++ src/pages/ProductView.vue
@@ -4,19 +4,47 @@ import { ref } from "vue";
 import type { Product } from "../types/products";
 
 import makeRequest from "../utils/makeRequest";
+import type { Ref } from "vue";
 
 const products = ref<Product[]>([]);
 
-makeRequest({
-  method: "get",
-  url: "https://financialmodelingprep.com/api/v3/profile/AAPL,NVDA?apikey=39c41689f9fab5f0dcf71b542172366c00",
-}).then(({ data }) => {
-  products.value = data;
-});
+let paramsId = ref<any>();
+
+function makeClickRequest(id: Ref<any>) {
+  paramsId = id;
+  makeRequest({
+    method: "get",
+    url: `https://financialmodelingprep.com/api/v3/profile/${paramsId}?apikey=39c41689f9fab5f0dcf71b542172366c`,
+  }).then(({ data }) => {
+    products.value = data;
+  });
+
+  console.log(paramsId + "ee111");
+}
+
+console.log(paramsId + "ee");
+
+// makeRequest({
+//   method: "get",
+//   url: "https://financialmodelingprep.com/api/v3/profile/AAPL,NVDA?apikey=39c41689f9fab5f0dcf71b542172366c00",
+// }).then(({ data }) => {
+//   products.value = data;
+// });
 </script>
 
 <template>
-  <h3>{{ $route.params.id }}</h3>
+  <h3>{{ (paramsId = $route.params.id) }}</h3>
+  <button @click="makeClickRequest(paramsId)">Load real data</button>
+  <div v-for="product in products">
+    <span>{{ product.companyName }}</span>
+    <sup>{{ product.price }}$</sup>
+    <div>Описание:{{ product.description }}</div>
+  </div>
+  <el-button type="primary" plain @click="$router.push({ name: 'List' })"
+    >Назад</el-button
+  >
+
+  <!-- <h3>{{ (paramsId = $route.params.id) }}</h3>
   <div v-for="product in products">
     <div v-if="product.symbol === $route.params.id">
       <span>{{ product.symbol }}</span>
@@ -27,7 +55,7 @@ makeRequest({
   </div>
   <el-button type="primary" plain @click="$router.push({ name: 'List' })"
     >Назад</el-button
-  >
+  > -->
 </template>
 
 <style scoped lang="scss"></style>

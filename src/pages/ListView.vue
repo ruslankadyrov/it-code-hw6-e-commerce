f0c2d4610ed9041141b902ebf8b655d1a18efe6f
kadyrov23 <zloyrudik@mail.ru>
2023-05-29 20:36:39 +0500

    search

--- src/pages/ListView.vue
+++ src/pages/ListView.vue
@@ -14,6 +14,8 @@ const productSymbol = ref("");
 const productStore = useProductStore();
 productStore.fetchProducts();
 
+let makeSearch = ref("false");
+
 // makeRequest({
 //   method: "get",
 //   url: "https://financialmodelingprep.com/api/v3/profile/AAPL,NVDA,YNDX,MSFT,BA,KO,IBM,V,AXP,F,INTC,EBAY,DELL,AMZN?apikey=39c41689f9fab5f0dcf71b542172366c",
@@ -21,15 +23,21 @@ productStore.fetchProducts();
 //   products.value = data;
 // });
 
-const symbolDebounce = useDebounce(productSymbol, 2000);
+const symbolDebounce = useDebounce(productSymbol, 1000);
 
 function makeClickRequest() {
+  makeSearch.value = "true";
   makeRequest({
     method: "get",
-    url: `https://financialmodelingprep.com/api/v3/profile/${symbolDebounce.value}?apikey=39c41689f9fab5f0dcf71b542172366c00`,
+    url: `https://financialmodelingprep.com/api/v3/profile/${symbolDebounce.value}?apikey=39c41689f9fab5f0dcf71b542172366c`,
   }).then(({ data }) => {
     products.value = data;
   });
+}
+
+function resetFilter() {
+  makeSearch.value = "false";
+  productSymbol.value = "";
 }
 
 function makeFilterRequest(symbol: string) {
@@ -53,18 +61,36 @@ const load = () => {
   <el-button type="primary" plain @click="makeClickRequest()">
     Search
   </el-button>
+  <el-button type="primary" plain @click="resetFilter()">
+    Reset filter
+  </el-button>
 
   <h3>List of stocks</h3>
-  <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
-    <li v-for="i in count" :key="i" class="infinite-list-item">
-      <el-image
-        style="width: 40px; height: 40px"
-        :src="productStore.products[i - 1].image"
-      />
-      {{ productStore.products[i - 1].symbol }}
-      {{ i }}
-    </li>
-  </ul>
+  <div v-if="makeSearch == 'false'">
+    <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+      <li v-for="i in count" :key="i" class="infinite-list-item">
+        <el-image
+          style="width: 40px; height: 40px"
+          :src="productStore.products[i - 1].image"
+        />
+        {{ productStore.products[i - 1].symbol }}
+        {{ i }}
+      </li>
+    </ul>
+  </div>
+
+  <div v-else="makeSearch == 'true'">
+    <el-scrollbar>
+      <p
+        v-for="product in products"
+        class="scrollbar-demo-item"
+        @click="$router.push('/product/' + product.symbol)"
+      >
+        <el-image style="width: 40px; height: 40px" :src="product.image" lazy />
+        {{ product.companyName }}
+      </p>
+    </el-scrollbar>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -85,5 +111,18 @@ const load = () => {
 }
 .infinite-list .infinite-list-item + .list-item {
   margin-top: 10px;
+}
+
+.scrollbar-demo-item {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
 }
 </style>

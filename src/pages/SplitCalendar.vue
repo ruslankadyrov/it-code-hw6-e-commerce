b90bc4edb8b584597745526c687b51d57ec920ce
kadyrov23 <zloyrudik@mail.ru>
2023-05-31 02:53:38 +0500

    datepicker

--- src/pages/SplitCalendar.vue
+++ src/pages/SplitCalendar.vue
@@ -1,5 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
 
-<template></template>
+const value1 = ref("");
+const value2 = ref("");
+</script>
 
-<style scoped lang="scss"></style>
+<template>
+  <h1>Split calendar</h1>
+  <div class="demo-date-picker">
+    <div class="block">
+      <div class="demonstration">From: {{ value1 }}</div>
+      <el-date-picker
+        v-model="value1"
+        type="date"
+        placeholder="Pick a Date"
+        format="YYYY/MM/DD"
+        value-format="YYYY-MM-DD"
+      />
+    </div>
+
+    <div class="block">
+      <div class="demonstration">To: {{ value2 }}</div>
+      <el-date-picker
+        v-model="value2"
+        type="date"
+        placeholder="Pick a Date"
+        format="YYYY/MM/DD"
+        value-format="YYYY-MM-DD"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.demo-date-picker {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+.demo-date-picker .block {
+  padding: 20px 0;
+  text-align: center;
+}
+.demo-date-picker .block:last-child {
+  border-right: none;
+}
+.demo-date-picker .demonstration {
+  font-size: 16px;
+}
+</style>
